21553badca4700bb0a59cd6c11b64a783e978718
Hafiza Laiba Qasim <laiba.qasim@192.168.1.5>
2025-07-28 22:20:28 +0500

    Initial React Repo Setup

---
+++ src/components/Footer/Footer.tsx
@@ -0,0 +1,12 @@
+export const Footer = () => {
+  return (
+    <nav>
+      <h1>Footer</h1>
+      <ul>
+        <li>Home</li>
+        <li>About</li>
+        <li>Contact</li>
+      </ul>
+    </nav>
+  );
+}
---
+++ src/components/Navbar/Navbar.tsx
@@ -0,0 +1,12 @@
+export const Navbar = () => {
+  return (
+    <nav>
+      <h1>Navbar</h1>
+      <ul>
+        <li>Home</li>
+        <li>About</li>
+        <li>Contact</li>
+      </ul>
+    </nav>
+  );
+}
